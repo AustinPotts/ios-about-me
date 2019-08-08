9ad3f8b950e72b555f26b79513c8fafde2f94ef0
Austin Potts <apotts@Austins-MacBook-Pro.local>
2019-08-08 13:00:58 -0400

    Implemented Button

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var HobbiesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        nameLabel.text = nil
+        HobbiesLabel.text = nil
+    }
+    
+    @IBAction func introduceYourself(_ sender: UIButton){
+        nameLabel.text = "Austin"
+        HobbiesLabel.text = "Making Apps, MMA, Fencing"
     }
 
 
